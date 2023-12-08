39f0a8389e7775c22203aeb44fc10800c22cb141
Divya21146 <113895154+Divya21146@users.noreply.github.com>
2023-12-08 18:09:36 +0530

    Update functions.php

--- functions.php
+++ functions.php
@@ -2,9 +2,9 @@
 function update_table_column_size() {
     global $wpdb;
 
-    $table_name = $wpdb->prefix . "table_name";  // replace with your table name
+    $table_name = $wpdb->prefix . "your_table_name";  // replace with your table name
 
-    $result = $wpdb->query("ALTER TABLE $table_name MODIFY COLUMN column_name VARCHAR(2000);");  //replace with your column name and change the varchar size as much as you want.
+    $result = $wpdb->query("ALTER TABLE $table_name MODIFY COLUMN your_column_name VARCHAR(2000);");  //replace with your column name and change the varchar size as much as you want.
 
     if ($result === false) {
         echo "Error: " . $wpdb->last_error;
